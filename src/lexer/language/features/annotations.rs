6b54a2458b24d22601f7487e524620b1fb949bb3
Gio <lotuspar0@gmail.com>
2023-09-13 00:30:05 +0800

    lexer: don't append annotation marker

--- src/lexer/language/features/annotations.rs
+++ src/lexer/language/features/annotations.rs
@@ -9,18 +9,24 @@ impl<'a> Lexer<'a> {
     /// Parses an annotation
     /// Assumes the iterator is on an annotation start character (@)
     pub fn annotation(&mut self) {
-        let start = self.offset();
+        let token_start = self.offset();
 
         set_error_unless!(
             self, Error::unrecoverable(ErrorKind::UnexpectedCurrentCharacter),
             Some(FEATURE_ANNOTATION)
         );
 
+        // Skip the first token
+        self.next();
+        let data_start = self.offset();
+
         read! { self,
             (Some(' ' | '\n' | '\r' | '(') | None) => {
+                let end = self.offset();
                 self.set_token_kind(TokenKind::LanguageAnnotation)
-                    .end_token_here(start)
-                    .make_token_symbol();
+                    .set_token_pos(data_start, end)
+                    .make_token_symbol()
+                    .set_token_pos(token_start, end);
                 break;
             },
             _ => {}
